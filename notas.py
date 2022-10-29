4b83b29eb6d79f60d7afef1165ce219dc989f209
jocarsa <info@josevicentecarratala.com>
2022-10-29 15:06:28 +0200

    Antialias Windows

--- notas.py
+++ notas.py
@@ -2,4 +2,11 @@ import tkinter as tk                        # Importo la librería de GUI
 
 raiz = tk.Tk()                              # Creo una interfaz gráfica de usuario
 
-raiz.mainloop()                             # Detiene la ejecución y previene que la ventana se cierre
+try:                                        # Intento ejecutar
+    from ctypes import windll               # Importo la libreria específica de GUI de Windows
+    windll.shcore.SetProcessDpiAwareness(1) # Activo el antialias para texto etc dentro de las interfaces
+except Exception as e:                      # Atrapo la excepción en caso de que se produzca
+    print(e)                                # Saco la excepción por pantalla
+finally:                                    # En todo caso:
+    raiz.mainloop()                         # Detiene la ejecución y previene que la ventana se cierre      
+
