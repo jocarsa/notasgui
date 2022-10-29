5f3fa36ccfb20c899399c8d6abef47928504cb81
jocarsa <info@josevicentecarratala.com>
2022-10-29 15:01:24 +0200

    Creo ventana inicial

---
+++ notas.py
@@ -0,0 +1,5 @@
+import tkinter as tk                        # Importo la librería de GUI
+
+raiz = tk.Tk()                              # Creo una interfaz gráfica de usuario
+
+raiz.mainloop()                             # Detiene la ejecución y previene que la ventana se cierre
