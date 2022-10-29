23ba220b22a0728d4fb712858e83b04963543695
jocarsa <info@josevicentecarratala.com>
2022-10-29 15:10:48 +0200

    Geometria de la vetana

--- notas.py
+++ notas.py
@@ -1,7 +1,8 @@
 import tkinter as tk                        # Importo la librería de GUI
 
 raiz = tk.Tk()                              # Creo una interfaz gráfica de usuario
-raiz.title("Notas v0.01")                   # Especifico el título de la ventana 
+raiz.title("Notas v0.01")                   # Especifico el título de la ventana
+raiz.geometry('200x200+20+50')              # Geomtria de la ventana y margen con la pantalla
 
 try:                                        # Intento ejecutar
     from ctypes import windll               # Importo la libreria específica de GUI de Windows
