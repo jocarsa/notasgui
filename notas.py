8e07694b0e615f8a85f64bc5290e6987918bf5c8
jocarsa <info@josevicentecarratala.com>
2022-10-29 15:56:29 +0200

    SQLite crear tablas

--- notas.py
+++ notas.py
@@ -1,5 +1,31 @@
 import tkinter as tk                        # Importo la librería de GUI
 from tkinter import ttk                     # Importo la nueva librería TTK
+import sqlite3 as bd                                                    # Importo la librería SQLiteimport sqlite3 as bd                                                    # Importo la librería SQLite
+
+# CONEXIÓN INICIAL CON LA BASE DE DATOS
+
+conexion = bd.connect("notas.sqlite")                                   # Indico el nombre de la base de datos
+cursor = conexion.cursor()                                              # Creo un cursor
+# Sobre el cursor, ejecuto una petición para crear una tabla en la base de datos en el caso de que no exista anteriormente
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS 'notas' (
+        'id' INTEGER,
+        'texto' TEXT,
+        'color' TEXT,
+        'fecha' TEXT,
+        PRIMARY KEY('id' AUTOINCREMENT)
+    );
+""")
+# Sobre el cursor, ejecuto una petición para crear una tabla de usuarios en el caso de que no exista
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS 'usuarios' (
+        'id' INTEGER,
+        'usuario' TEXT,
+        'contrasena' TEXT,
+        'email' TEXT,
+        PRIMARY KEY('id' AUTOINCREMENT)
+    );
+""")
 
 # DECLARO FUNCIONES PARA EL PROGRAMA
 
